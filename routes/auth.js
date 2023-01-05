2e4fff18833e0ed291d0388b9327bd99ce4257e4
bappyasif <bappyasif@hotmail.com>
2023-01-06 01:08:46 +0600

    fixed bugs
    * adjusted passportJS auth redirected successfull routes
    * adjusted passportJS auth redirected failure routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,10 @@ const express = require("express");
 const { registerUser, loginUser, logoutUser, returnAuthenticatedUser, loginWithOauthProvider, extractDataForAnAuthenticatedUser, authenticatedUserJwtVerification } = require("../controllers/auth");
 const authRoutes = express();
 
+// const baseUrlForAuth = "http://localhost:3001"
+
+const baseUrlForAuth = "https://odbo-live.vercel.app"
+
 let isAuthenticated = (req, res, next) => {
     if (req.user || req?.session?.passport?.user || req?.jwt) {
         console.log("user authenticated!!")
@@ -22,29 +26,29 @@ authRoutes.get("/auth/google", passport.authenticate("google", {
 }))
 authRoutes.get("/auth/google/redirect", passport.authenticate("google", {
     failureMessage: "Login error",
-    failureRedirect: "http://localhost:3001/login",
-    successRedirect: "http://localhost:3001/login/success"
+    failureRedirect: `${baseUrlForAuth}/login`,
+    successRedirect: `${baseUrlForAuth}/login/success`
 }))
 
 authRoutes.get("/auth/facebook", passport.authenticate("facebook", { scope : ['email', "public_profile"]}))
 authRoutes.get("/auth/facebook/redirect", passport.authenticate("facebook", {
     failureMessage: "Login error",
-    failureRedirect: "http://localhost:3001/login",
-    successRedirect: "http://localhost:3001/login/success"
+    failureRedirect: `${baseUrlForAuth}/login`,
+    successRedirect: `${baseUrlForAuth}/login/success`
 }))
 
 authRoutes.get('/auth/github',passport.authenticate('github',  { scope: [ 'user:email' ] }));
 authRoutes.get('/auth/github/redirect', passport.authenticate('github', { 
     failureMessage: "Login error",
-    failureRedirect: "http://localhost:3001/login",
-    successRedirect: "http://localhost:3001/login/success"
+    failureRedirect: `${baseUrlForAuth}/login`,
+    successRedirect: `${baseUrlForAuth}/login/success`
 }));
 
 authRoutes.get('/auth/twitter',passport.authenticate('twitter'));
 authRoutes.get('/auth/twitter/redirect', passport.authenticate('twitter', { 
     failureMessage: "Login error",
-    failureRedirect: "http://localhost:3001/login",
-    successRedirect: "http://localhost:3001/login/success"
+    failureRedirect: `${baseUrlForAuth}/login`,
+    successRedirect: `${baseUrlForAuth}/login/success`
 }));
 
 authRoutes.get("/login/success", isAuthenticated, returnAuthenticatedUser)
