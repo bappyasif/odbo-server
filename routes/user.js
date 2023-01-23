3974e165a11948dbf4ca449fa82d1ad0cd9ff0a3
bappyasif <bappyasif@hotmail.com>
2023-01-24 01:20:57 +0600

    added security enabled routes for user

--- routes/user.js
+++ routes/user.js
@@ -4,18 +4,25 @@ const { getAllUsers, getAnUser, updateUser, deleteUser, acceptUserFriendRequest,
 const { isAuthenticated } = require("./auth");
 const userRoutes = express();
 
-userRoutes.get("/", isAuthenticated, getAllUsers)
+// userRoutes.get("/", isAuthenticated, getAllUsers)
+userRoutes.get("/", authenticatedUserJwtVerification, getAllUsers)
 userRoutes.get("/:userId/publicPayload", getAnUserWithMinimumData);
 // userRoutes.get("/:userId", isAuthenticated, getAnUser)
 userRoutes.get("/:userId", authenticatedUserJwtVerification, getAnUser)
 
-userRoutes.put("/:userId/profile", isAuthenticated, updateUserProfileInfo)
-userRoutes.put("/:userId", isAuthenticated, updateUser)
-userRoutes.delete("/:userId", isAuthenticated, deleteUser)
+// userRoutes.put("/:userId/profile", isAuthenticated, updateUserProfileInfo)
+// userRoutes.put("/:userId", isAuthenticated, updateUser)
+// userRoutes.delete("/:userId", isAuthenticated, deleteUser)
+userRoutes.put("/:userId/profile", authenticatedUserJwtVerification, updateUserProfileInfo)
+userRoutes.put("/:userId", authenticatedUserJwtVerification, updateUser)
+userRoutes.delete("/:userId", authenticatedUserJwtVerification, deleteUser)
 
-userRoutes.put("/:userId/accept", acceptUserFriendRequest)
-userRoutes.put("/:userId/reject", rejectUserFriendRequest)
+// userRoutes.put("/:userId/accept", acceptUserFriendRequest)
+// userRoutes.put("/:userId/reject", rejectUserFriendRequest)
+userRoutes.put("/:userId/accept", authenticatedUserJwtVerification, acceptUserFriendRequest)
+userRoutes.put("/:userId/reject", authenticatedUserJwtVerification, rejectUserFriendRequest)
 
-userRoutes.put("/:userId/remove", isAuthenticated, removeUserFromFriendList)
+// userRoutes.put("/:userId/remove", isAuthenticated, removeUserFromFriendList)
+userRoutes.put("/:userId/remove", authenticatedUserJwtVerification, removeUserFromFriendList)
 
 module.exports = userRoutes;
