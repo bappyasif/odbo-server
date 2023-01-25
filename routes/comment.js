47db69531d25647e3d8d731dab6394e242b7afad
bappyasif <bappyasif@hotmail.com>
2023-01-25 23:18:33 +0600

    added authentication to comments
    * jwt token based authentication is now in place for comments data protection from outside of app

--- routes/comment.js
+++ routes/comment.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const { authenticatedUserJwtVerification } = require("../controllers/auth");
 const { getAllComments, getSoloComment, deleteSoloComment, updateSoloCommentCounts, createNewComment, getAllCommentsFromSinglePost, updateSoloCommentText } = require("../controllers/comment");
 const commentRoutes = express();
 
-commentRoutes.get("/", getAllComments);
+commentRoutes.get("/", authenticatedUserJwtVerification, getAllComments);
 
 commentRoutes.get("/:commentId", getSoloComment);
-commentRoutes.delete("/:commentId", deleteSoloComment);
-commentRoutes.put("/:commentId", updateSoloCommentCounts);
-commentRoutes.put("/:commentId/text", updateSoloCommentText);
+// commentRoutes.delete("/:commentId", deleteSoloComment);
+commentRoutes.delete("/:commentId", authenticatedUserJwtVerification, deleteSoloComment);
 
-commentRoutes.post("/create/new", createNewComment);
+// commentRoutes.put("/:commentId", updateSoloCommentCounts);
+commentRoutes.put("/:commentId", authenticatedUserJwtVerification, updateSoloCommentCounts);
+
+commentRoutes.put("/:commentId/text", authenticatedUserJwtVerification, updateSoloCommentText);
+
+commentRoutes.post("/create/new", authenticatedUserJwtVerification, createNewComment);
 
 commentRoutes.get("/post/:postId", getAllCommentsFromSinglePost);
 
