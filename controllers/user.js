792341c6081166c3fc31db6b370d67b044f097f9
bappyasif <bappyasif@hotmail.com>
2023-02-01 22:32:39 +0600

    fixed bug
    * reset password now uses salt and hashing updates as well, which is checked by login route to verify user correctly with that updated password

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ const async = require("async");
 const User = require("../models/user");
 const Post = require("../models/post");
 const Comment = require("../models/comment");
+const { generatePassword } = require("../utils/jwt");
 
 const getAllUsers = (req, res, next) => {
     User.find({})
@@ -126,11 +127,12 @@ const resetUserAccountPassword = [
     body("confirm-password", "can not be empty").isLength({ min: 1 }).escape(),
     body("confirm-password", "new password confirmation does not match")
         .custom((val, { req }) => {
+            // console.log(val === req.body["new-password"], val, req.body["new-password"])
             return val === req.body["new-password"]
         }),
     (req, res) => {
         const errors = validationResult(req)
-        
+
         if (!errors.isEmpty()) {
             console.log("errors!!", errors.array())
             return res.status(401).json({ msg: "error occured", errors: errors.array() })
@@ -143,15 +145,22 @@ const resetUserAccountPassword = [
                 // console.log(userId, val, currentUser.password, currentUser.password === val)
                 console.log(currentUser.password === req.body["current-password"], currentUser.password, req.body["current-password"])
                 if (currentUser.password === req.body["current-password"]) {
-                    
+
                     console.log("password matched!!", req.body["new-password"])
+                    
+                    const getSaltAndHash = generatePassword(req.body["new-password"]);
+
+                    const salt = getSaltAndHash.salt;
+                    const hash = getSaltAndHash.hash;
 
                     currentUser.password = req.body["new-password"]
-                    
+                    currentUser.salt = salt;
+                    currentUser.hash = hash;
+
                     User.findByIdAndUpdate(currentUser._id, currentUser, {})
-                    .then(() => {
-                        res.status(200).json({ msg: "password changed!!" })
-                    }).catch(err => console.log(err, "updated failed!!"))
+                        .then(() => {
+                            res.status(200).json({ msg: "password changed!!" })
+                        }).catch(err => console.log(err, "updated failed!!"))
 
                     // console.log("all good!!", req.body)
                     // res.status(200).json({ msg: "password changed!!" })
